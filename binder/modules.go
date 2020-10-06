998cfce80b2d60a7762e1db4ce2bb9f398392667
Chuck Jazdzewski <chuckj@google.com>
2020-10-05 21:12:15 -0700

    Add source modules

---
+++ binder/modules.go
@@ -0,0 +1,49 @@
+package binder
+
+import (
+	"io"
+)
+
+// ModuleSource is a source file for a module
+type ModuleSource interface {
+    // Name is the name the module that appears in the scope
+	Name() string
+
+    // FileName is the file name use in  error messages
+	FileName() string
+
+    // NewReader create a reader for the source
+	NewReader() (io.Reader, error)
+}
+
+// NewModuleSource creates a new ModuleSource
+func NewModuleSource(name string, fileName string, readFactory func() (io.Reader, error)) ModuleSource {
+	return &moduleSource{name: name, fileName: fileName, readFactory: readFactory}
+}
+
+// ModuleSourceScope is a scope for finding module sources
+type ModuleSourceScope interface {
+    // FindSocpe finds a subscope of a module scope
+	FindScope(name string) (ModuleSourceScope, error)
+
+    // Find a source module in a scope 
+	Find(name string) (ModuleSource, error)
+}
+
+type moduleSource struct {
+	name        string
+	fileName    string
+	readFactory func() (io.Reader, error)
+}
+
+func (m *moduleSource) Name() string {
+	return m.name
+}
+
+func (m *moduleSource) FileName() string {
+	return m.fileName
+}
+
+func (m *moduleSource) NewReader() (io.Reader, error) {
+	return m.readFactory()
+}
